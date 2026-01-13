26d2acdaec424ad614625e688a884c67fcaba540
Daiana <daiana@MacBook-Pro-Daiana.local>
2026-01-13 16:22:04 +0600

    added sout and VisaPayment class

--- src/kg/sim_bank/payment/QrPayment.java
+++ src/kg/sim_bank/payment/QrPayment.java
@@ -3,7 +3,7 @@ package kg.sim_bank.payment;
 public class QrPayment {
     public static void main(String[] args) {
         System.out.println("QR payment");
-        
+        System.out.println("hello world");
     }
 
 }
