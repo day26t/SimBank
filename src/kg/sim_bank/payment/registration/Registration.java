6e3ece6d5414ab1e2811cd7b9a90f4664749dce9
Daiana <daiana@MacBook-Pro-Daiana.local>
2026-01-14 09:44:45 +0600

    Registration -3

--- src/kg/sim_bank/payment/registration/Registration.java
+++ src/kg/sim_bank/payment/registration/Registration.java
@@ -4,5 +4,6 @@ public class Registration {
     public static void main(String[] args) {
         System.out.println("registration");
         System.out.println("registration - v2");
+        System.out.println("registration - v3");
     }
 }
