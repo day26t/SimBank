04aeb7cd8a055d52c4911f0b831634121fc37815
Daiana <daiana@MacBook-Pro-Daiana.local>
2026-01-14 10:24:50 +0600

    Resolved conflict

---
+++ src/kg/sim_bank/payment/delivery/Delivery.java
@@ -0,0 +1,8 @@
+package kg.sim_bank.payment.delivery;
+
+public class Delivery {
+    public static void main(String[] args) {
+        System.out.println("Delivery Yandex");
+        System.out.println("Delivery Glovo");
+    }
+}
