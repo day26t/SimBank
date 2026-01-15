e8c847dbccb09e0a51e59d10d0c60cdacd0521cd
Daiana <daiana@MacBook-Pro-Daiana.local>
2026-01-15 09:36:56 +0600

    January promo.
    January Promotion1
    
    January Promotion2
    
    January Promotion 3

---
+++ src/kg/sim_bank/payment/promotions/January.java
@@ -0,0 +1,13 @@
+package kg.sim_bank.payment.promotions;
+
+public class January {
+    public static void main(String[] args) {
+        System.out.println("January Promotion");
+        System.out.println("January Promotion123");
+        System.out.println("January Promotion1234");
+        System.out.println("January Promotion12345");
+        System.out.println("January Promotion12345ok");
+        System.out.println("January Promotion12345jan");
+
+    }
+}
